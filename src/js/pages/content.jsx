ddf100a139b3c03507d60a3c02282401ab8def59
ayuub2 <ayuubadan@outlook.com>
2016-08-30 10:45:09 +0100

    starting content table design

--- src/js/pages/content.jsx
+++ src/js/pages/content.jsx
@@ -15,8 +15,21 @@ onClick:function(ev){
   render:function(){
     return (
       <Page>
-        <BasicSegment nopadding style={{height:"30%", width:"100%"}}>
-          <img src="http://www.essex.ac.uk/campusm/edits/content-page/contentarrow.jpg" height="100%" width="100%"/>
+        <BasicSegment nopadding style={{height:"40%", width:"100%"}}>
+          <img src="http://www.essex.ac.uk/campusm/edits/content-page/contentarrow.jpg" height="100%" width="100%" style={{position:"absolute", marginTop:"-40px"}}/>
+        </BasicSegment>
+        <BasicSegment style={{position:"absolute", top:"50%", left:"50%", marginTop:"-120px", marginLeft:"-135px", width:"300px", height:"400px"}}>
+            <h2 style={{color:"#33ccff", fontWeight:"bold"}}>LEARNING</h2>
+            <table style={{width:"100%", marginTop:"-20px"}}>
+              <tr>
+                <th style={{color:"#33ccff", fontSize:"135%"}}>06</th>
+                <th style={{fontSize:"120%"}}>Your studies</th>
+              </tr>
+              <tr>
+                <th style={{color:"#33ccff", fontSize:"135%"}}>10</th>
+                <th style={{fontSize:"120%"}}>Tech World</th>
+              </tr>
+            </table>
         </BasicSegment>
       </Page>
     );
