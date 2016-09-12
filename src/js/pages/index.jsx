97f89ca96e30f7f8dff4be2f5c8ec7df051a636b
ayuub2 <ayuubadan@outlook.com>
2016-09-12 15:33:34 +0100

    Version 1 Complete

--- src/js/pages/index.jsx
+++ src/js/pages/index.jsx
@@ -1,13 +1,11 @@
 var React = require("-aek/react");
-var Page = require("-components/page");
-var {BasicSegment} = require("-components/segment");
 var Button = require("-components/button");
 
 
 
 var IndexPage = React.createClass({
   onClick:function(page,ev){
-    console.log("page: " + page);
+
     ev.preventDefault();
     this.props.onSelect(page);
   },
