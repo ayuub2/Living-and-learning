8d92ffa4cd2be9dffe7c2ead9ba6162d7a8c8727
John Maydew <johnmaydew@gmail.com>
2016-09-22 14:27:01 +0100

    Fix for iPad button touch issues

--- src/js/pages/content.jsx
+++ src/js/pages/content.jsx
@@ -46,11 +46,15 @@ var ContentPage = React.createClass({
     console.error(error);
   },
 
-  onClick:function(page,ev){
-    ev.preventDefault();
-    this.props.onSelect(page);
+  /**
+    http://stackoverflow.com/questions/20377837/how-to-access-custom-attributes-from-event-object-in-react
+  */
+  onSelectMenu: function(page) {
+    return function(event) {
+      event.preventDefault();
+      this.props.onSelect(page);
+    }.bind(this);
   },
-
 
   render:function(){
 
@@ -64,7 +68,7 @@ var ContentPage = React.createClass({
             <Segment variation="vertical">Fetching your location...</Segment>
           </Segment>
         </Page>
-      )
+      );
     } else {
       var city = this.state.location.name;
 
@@ -80,73 +84,73 @@ var ContentPage = React.createClass({
                 <table  style={{width:"100%", marginTop:"-20px"}}>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%", width:"30px"}}>01</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"study_southend")} style={{display:"block"}}>Your studies</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("study_southend")} onTouchStart={this.onSelectMenu("study_southend")} style={{display:"block"}}>Your studies</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%"}}>05</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"tech_southend")} style={{display:"block"}}>Tech World</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("tech_southend")} onTouchStart={this.onSelectMenu("tech_southend")} style={{display:"block"}}>Tech World</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%"}}>09</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"health_southend")} style={{display:"block"}}>Staying health and happy</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("health_southend")} onTouchStart={this.onSelectMenu("health_southend")} style={{display:"block"}}>Staying health and happy</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%"}}>17</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"career_southend")}>Future career starts here</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("career_southend")} onTouchStart={this.onSelectMenu("career_southend")}>Future career starts here</a></th>
                   </tr>
                 </table>
                 <h2 style={{color:"#2FFB3C", fontWeight:"bold"}}>Student Union</h2>
                 <table  style={{width:"100%", marginTop:"-20px"}}>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>20</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su")}>Hello wonderful people</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su")} onTouchStart={this.onSelectMenu("su")}>Hello wonderful people</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>21</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su_two")}>What do we do?</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su_two")} onTouchStart={this.onSelectMenu("su_two")}>What do we do?</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>22</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su_three")}>10 reasons why</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su_three")} onTouchStart={this.onSelectMenu("su_three")}>10 reasons why</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>23</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su_four")}>This is just the start</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su_four")} onTouchStart={this.onSelectMenu("su_four")}>This is just the start</a></th>
                   </tr>
                 </table>
                 <h2 style={{color:"#ff3399", fontWeight:"bold"}}>LIVING</h2>
                 <table  style={{width:"100%", marginTop:"-20px"}}>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%", width:"30px"}}>24</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"welcome_southend")}>Welcome Home</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("welcome_southend")} onTouchStart={this.onSelectMenu("welcome_southend")}>Welcome Home</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>28</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"sports_southend")}>Sports at Essex</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("sports_southend")} onTouchStart={this.onSelectMenu("sports_southend")}>Sports at Essex</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>33</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"enjoy_southend")}>Enjoy yourself</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("enjoy_southend")} onTouchStart={this.onSelectMenu("enjoy_southend")}>Enjoy yourself</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>33</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"money_southend")}>Money matters</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("money_southend")} onTouchStart={this.onSelectMenu("money_southend")}>Money matters</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>37</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"eat_southend")}>Eat, drink, buy</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("eat_southend")} onTouchStart={this.onSelectMenu("eat_southend")}>Eat, drink, buy</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>44</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"map_southend")}>Maps</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("map_southend")} onTouchStart={this.onSelectMenu("map_southend")}>Maps</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>46</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"news_southend")}>News and media</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("news_southend")} onTouchStart={this.onSelectMenu("news_southend")}>News and media</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>49</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"essex_southend")}>Essex is for life</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("essex_southend")} onTouchStart={this.onSelectMenu("essex_southend")}>Essex is for life</a></th>
                   </tr>
                 </table>
             </BasicSegment>
@@ -164,81 +168,81 @@ var ContentPage = React.createClass({
                 <table  style={{width:"100%", marginTop:"-20px"}}>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%", width:"30px"}}>01</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"study")} style={{display:"block"}}>Your studies</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("study")} onTouchStart={this.onSelectMenu("study")} style={{display:"block"}}>Your studies</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%"}}>05</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"tech_one")} style={{display:"block"}}>Tech World</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("tech_one")} onTouchStart={this.onSelectMenu("tech_one")} style={{display:"block"}}>Tech World</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%"}}>09</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"health")} style={{display:"block"}}>Staying health and happy</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("health")} onTouchStart={this.onSelectMenu("health")} style={{display:"block"}}>Staying health and happy</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#33ccff", fontSize:"135%"}}>17</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"career")}>Future career starts here</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("career")} onTouchStart={this.onSelectMenu("career")}>Future career starts here</a></th>
                   </tr>
                 </table>
                 <h2 style={{color:"#2FFB3C", fontWeight:"bold"}}>Student Union</h2>
                 <table  style={{width:"100%", marginTop:"-20px"}}>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>20</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su")}>Hello wonderful people</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su")} onTouchStart={this.onSelectMenu("su")}>Hello wonderful people</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>21</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su_two")}>What do we do?</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su_two")} onTouchStart={this.onSelectMenu("su_two")}>What do we do?</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>22</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su_three")}>10 reasons why</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su_three")} onTouchStart={this.onSelectMenu("su_three")}>10 reasons why</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#2FFB3C", fontSize:"135%", width:"30px"}}>23</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"su_four")}>This is just the start</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("su_four")} onTouchStart={this.onSelectMenu("su_four")}>This is just the start</a></th>
                   </tr>
                 </table>
                 <h2 style={{color:"#ff3399", fontWeight:"bold"}}>LIVING</h2>
                 <table  style={{width:"100%", marginTop:"-20px"}}>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%", width:"30px"}}>24</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"welcome")}>Welcome Home</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("welcome")} onTouchStart={this.onSelectMenu("welcome")}>Welcome Home</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>28</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"sports")}>Sports at Essex</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("sports")} onTouchStart={this.onSelectMenu("sports")}>Sports at Essex</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>31</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"culture")}>Cultural Essex</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("culture")} onTouchStart={this.onSelectMenu("culture")}>Cultural Essex</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>33</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"money")}>Money matters</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("money")} onTouchStart={this.onSelectMenu("money")}>Money matters</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>37</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"eat")}>Eat, drink, buy</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("eat")} onTouchStart={this.onSelectMenu("eat")}>Eat, drink, buy</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>40</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"green")}>Green space</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("green")} onTouchStart={this.onSelectMenu("green")}>Green space</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>41</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"around")}>Get around</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("around")} onTouchStart={this.onSelectMenu("around")}>Get around</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>44</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"map")}>Maps</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("map")} onTouchStart={this.onSelectMenu("map")}>Maps</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>46</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"news")}>News and media</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("news")} onTouchStart={this.onSelectMenu("news")}>News and media</a></th>
                   </tr>
                   <tr>
                     <th style={{color:"#ff3399", fontSize:"135%"}}>49</th>
-                    <th style={{fontSize:"120%"}}><a onClick={this.onClick.bind(this,"essex")}>Essex is for life</a></th>
+                    <th style={{fontSize:"120%"}}><a onClick={this.onSelectMenu("essex")} onTouchStart={this.onSelectMenu("essex")}>Essex is for life</a></th>
                   </tr>
                 </table>
             </BasicSegment>
